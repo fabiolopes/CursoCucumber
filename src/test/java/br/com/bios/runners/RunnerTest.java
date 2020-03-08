dbd97dc07b4e76bc3a91380076477b09b4ea4091
Fabio Bione <fabiolopesbione@hotmail.com>
2020-03-07 23:06:54 -0300

    @BeforeClass para rodar antes de tudo Reuso de steps por contexto

--- src/test/java/br/com/bios/runners/RunnerTest.java
+++ src/test/java/br/com/bios/runners/RunnerTest.java
@@ -1,5 +1,9 @@
 package br.com.bios.runners;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.SnippetType;
@@ -17,4 +21,15 @@ import cucumber.api.junit.Cucumber;
 		dryRun = false)
 public class RunnerTest {
 
+	@BeforeClass
+	public static void reset() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://srbarriga.herokuapp.com/");
+		driver.findElement(By.id("email")).sendKeys("a@a");
+		driver.findElement(By.id("senha")).sendKeys("a");
+		driver.findElement(By.tagName("button")).click();
+		driver.findElement(By.linkText("reset")).click();
+		driver.quit();
+	}
 }
